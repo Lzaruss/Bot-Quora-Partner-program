bb53ecf26de8cc878380108f2c0bd623ee7028d9
Alberto Lazaro <104428151+lzaruss@users.noreply.github.com>
2022-12-29 15:54:52 +0100

    Args update
    In this update, I automate the login, in some cases it may give error because there is a captcha (although it does not usually appear), in that case you must remove the function -> "iniciar_sesion()".
    I am working on an added argument so you don't have to modify the code in such a case.

--- script.py
+++ script.py
@@ -2,6 +2,14 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 import time
+import json
+import argparse
+
+parser = argparse.ArgumentParser(add_help=False, description="Write -n or --num to set the number of questions you want the script to ask")
+parser.add_argument('-n', '--num', type=int, required=False, help="Number of questions you want for the script(if you dont say any number, by defect will be 10)")
+parser.add_argument('-h', '--help', action='help', help="Write -f or --func ['req', 'send'] to change the use of the script\nExample: python .\\test.py -f send")
+args = parser.parse_args()
+func_arg = args.func
 
 def request_question():
     filename = 'preguntas.txt'
@@ -16,31 +24,55 @@ def request_question():
     f.close
     return first_line
 
-driver = webdriver.Chrome()
+def iniciar_sesion():
+    credentials = cred_return()
+    driver.find_element(By.XPATH, '//*[@id="email"]').send_keys(credentials["email"])
 
-url = "https://es.quora.com/"
-driver.get(url)
+    time.sleep(0.5)
+    driver.find_element(By.XPATH, '//*[@id="password"]').send_keys(credentials["password"])
 
-#Tiempo de espera para que puedas iniciar sesion
-time.sleep(35)
-i = 0
-while i != 100:
+    time.sleep(0.5)
+    driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div/div/div/div/div/div[2]/div[2]/div[4]/button').click()
+
+def cred_return():
+    cred_file = open("./credentials.json", "r")
+    credentials = cred_file.read()
+    credentials = json.loads(credentials)
+    cred_file.close
+    return credentials
+
+#Unable LOGS
+options = webdriver.ChromeOptions() 
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+driver = webdriver.Chrome(options=options, executable_path=r'C:\WebDrivers\chromedriver.exe')
+driver.get("https://es.quora.com/")
+
+iniciar_sesion()
+
+#Por defecto pondremos 10 preguntas
+try:
+    numPreguntas = args.num
+except:
+    numPreguntas = 10
+preventiveNegativeQuestions = lambda num: 10 if num <= 0 else num
+
+num = preventiveNegativeQuestions(numPreguntas)
+print(f"Num of questions: {num}")
+while num != 0:
     try:
         clickable = driver.find_element(By.XPATH, "//*[@id='mainContent']/div/div/div[1]/div/div/div[1]/div/div[2]")
         ActionChains(driver)\
                 .click(clickable)\
                 .perform()
-        time.sleep(1)
-        textArea = driver.find_element(By.XPATH, "//*[@id='root']/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[1]/div/div[1]/div/div/div/div/div[2]/div/div/div[1]/div/div/textarea")
-        textArea.send_keys(request_question())
-        time.sleep(2)
-        #TODO poner la opcion para cuando ya se haya hecho esa pregunta
+        time.sleep(1.2)
+
+        driver.find_element(By.XPATH, "//*[@id='root']/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[1]/div/div[1]/div/div/div/div/div[2]/div/div/div[1]/div/div/textarea").send_keys(request_question())
+        time.sleep(2.2)
         #Le damos al boton de preguntar 
         driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[2]/div/div[2]/button[2]').click()
-        time.sleep(4)
-        #TODO en un futuro añadir la opcion de presionar los demas temas
+        time.sleep(3.2)
         driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div[2]/div/div/div/div[2]/div/div/div[2]/div[2]/div/div/button').click()
-        time.sleep(3)
+        time.sleep(2.2)
         #Pulsamos el boton de continuar sin editar los temas
         driver.find_element(By.XPATH, '//*[@id="root"]/div/div[4]/div[2]/div/div[3]/div[2]/button').click()
         time.sleep(4)
@@ -48,21 +80,18 @@ while i != 100:
         #Solicitar respuestas
         for x in range(1, 22):
             driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div[3]/div/div/div/div[2]/div/div/div[2]/div[1]/div/div[1]/div[3]/div[2]/div/div/div/div/div[1]/div[2]/div['+str(x)+']/div/div/div/div[3]/div/div/div').click()
-            time.sleep(0.2)
+            time.sleep(0.15)
 
         time.sleep(1)
 
         #Enviamos la pregunta
         driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div[3]/div/div/div/div[2]/div/div/div[2]/div[2]/div/div[2]/button').click()
-        time.sleep(1.5)
     except:
         pass
     time.sleep(1.5)
     driver.get("https://es.quora.com/")
-    i +=1
+    num -=1
+    print(f"\rQuestions left: {num} ", end="")
 
-
-
-time.sleep(30)
-
+time.sleep(5)
 driver.close()
